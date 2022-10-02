e7bdc5caecbdf900a73d62e0d0ffac719a250392
“[Shiwanshi <1ds20cs198@dsce.edu.in>
2022-10-02 21:20:37 +0530

    Added BFS in C language

---
+++ C/BreadthFirstSearch/BreadthFirstSearch.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void BFS (int a[10][10],int n,int src,int s[10])
+{
+ int f=0,r=-1,q[20],i,v;
+ printf ("Source Node: %d\n",src);
+ s[src] = 1;
+ q[++r] = src;
+ printf("Visited Nodes using BFS:\n");
+ while (f <= r)
+ {
+ v = q[f++];
+ for (i=1;i<=n;i++)
+ if (s[i]==0 && a[v][i])
+ {
+ q[++r] = i;
+ printf ("%d\t",i);
+ s[i] = 1;
+ }
+ }
+}
+int main ()
+{
+ int a[10][10],n,i,j,s[20],src;
+ printf ("Enter no. of Nodes in Graph\n");
+ scanf ("%d",&n);
+ printf ("Enter the connections of Graph in Adjacency Matrix\n");
+ for (i=1;i<=n;i++)
+ for (j=1;j<=n;j++)
+ scanf ("%d",&a[i][j]);
+ printf ("Enter the Source Node\n");
+ scanf ("%d",&src);
+ for(i=1;i<=n;i++)
+ s[i]=0;
+ BFS (a,n,src,s);
+ printf ("\n");
+ return 0;
+}
+/*OUTPUT:
+Enter no. of Nodes in Graph
+8
+Enter the connections of Graph in Adjacency Matrix
+0 1 0 1 0 0 1 0
+1 0 0 0 1 1 0 0
+0 0 0 0 0 1 0 1
+1 0 0 0 0 1 0 0
+0 1 0 0 0 0 1 0
+0 1 1 1 0 0 0 0 
+1 0 0 0 1 0 0 0
+0 0 1 0 0 0 0 0
+Enter the Source Node
+1
+Source Node = 1
+Visited Nodes using BFS:
+2 4 7 5 6 3 8
+*/
